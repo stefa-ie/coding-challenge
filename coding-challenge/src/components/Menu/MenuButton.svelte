3c320e1dc0353caacec5dacae52a301cb1835f2d
Work <nguyen.stfn@gmail.com>
2026-02-13 15:28:33 +0100

    added MenuButton

---
+++ coding-challenge/src/components/Menu/MenuButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { getContext } from "svelte";
+    import Button from "../Button/Button.svelte";
+
+    const { toggle } = getContext<{ toggle: () => void }>('menuContext');
+</script>
+
+<Button
+    type="button"
+    on:click={toggle}
+>
+    <slot />
+</Button>
+
