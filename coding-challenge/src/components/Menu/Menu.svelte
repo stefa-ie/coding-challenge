d24eada87af3b28f92480ebe95c2c26e2ab62e7b
Work <nguyen.stfn@gmail.com>
2026-02-13 14:26:42 +0100

    added file structure for component Menu and code for Menu.svelte subcomponent

---
+++ coding-challenge/src/components/Menu/Menu.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { setContext }  from "svelte";
+    import { writable, type Writable } from "svelte/store";
+
+    const open: Writable<boolean> = writable(false);
+
+    function toggle() {
+        open.update(v => !v);
+    }
+
+    // Providing values to children
+    setContext("menu", { open, toggle });
+</script>
+
+<div class="menu">
+    <slot />
+</div>
