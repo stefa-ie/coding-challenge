90e9838ae182a54cc6c64397bdb6c822b2d94d3a
Work <nguyen.stfn@gmail.com>
2026-02-13 15:15:11 +0100

    added general Button component

---
+++ coding-challenge/src/components/Button/Button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let onClick = () => {};
+    export let type: any = "button";
+    export let disabled: boolean = false;
+
+    const base = "w-[250px] h-[40px] rounded-lg bg-gray-100 flex items-center justify-between px-4"
+</script>
+
+<button 
+    type={type} 
+    class={base}
+    disabled={disabled}
+    on:click={onClick}>
+    <slot />
+</button>
